598211cb7c3bcdb499dcbea048b405387c33320f
dede65 <dede.ugur65@gmail.com>
2019-06-24 21:16:01 +0300

    navigate to PlayerDetails.js screen

--- src/components/SinglePlayer.js
+++ src/components/SinglePlayer.js
@@ -5,62 +5,66 @@ import PropTypes from "prop-types";
 
 const SinglePlayer = props => {
   return (
-      <TouchableOpacity style={styles.container}>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => {
+        this.props.navigation.navigate("PlayerDetailsScreen");
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: 8
+        }}
+      >
         <View
           style={{
-            flex:1,
             flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            paddingHorizontal: 8
+            alignItems: "center"
           }}
         >
-          <View
+          <Image
             style={{
-              flexDirection: "row",
-              alignItems: "center"
+              height: 60,
+              width: 60,
+              borderRadius: 30,
+              borderWidth: 0.5,
+              borderColor: "green",
+              margin: 4
             }}
-          >
-            <Image
-              style={{
-                height: 60,
-                width: 60,
-                borderRadius: 30,
-                borderWidth: 0.5,
-                borderColor: "green",
-                margin: 4
-              }}
-              source={{ uri: props.player.playerProfileImageURL }}
-            />
-            <View>
-              <Text style={{ marginVertical: 2, fontSize: 18 }}>
-                {props.player.firstName + " " + props.player.lastName}
-              </Text>
-              <Text style={{ marginVertical: 2, fontSize: 16 }}>
-                {props.player.position}
-              </Text>
-            </View>
-          </View>
+            source={{ uri: props.player.playerProfileImageURL }}
+          />
           <View>
-            <Icon name="chevron-right" color="green" size={32} />
+            <Text style={{ marginVertical: 2, fontSize: 18 }}>
+              {props.player.firstName + " " + props.player.lastName}
+            </Text>
+            <Text style={{ marginVertical: 2, fontSize: 16 }}>
+              {props.player.position}
+            </Text>
           </View>
         </View>
-      </TouchableOpacity>
+        <View>
+          <Icon name="chevron-right" color="green" size={32} />
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 export default SinglePlayer;
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal:8,
-    marginVertical:4,
+    marginHorizontal: 8,
+    marginVertical: 4,
     flexDirection: "row",
     borderColor: "green",
     borderRadius: 8,
     backgroundColor: "#fff",
-    elevation:8
-  },
+    elevation: 8
+  }
 });
