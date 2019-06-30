ffc3d07b939681e988506f240ad58f7ec55fcb5c
dede65 <dede.ugur65@gmail.com>
2019-07-01 01:38:35 +0300

    an example of push notification

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const firebase = require("react-native-firebase");
+//const firebase = require("react-native-firebase");
 
 admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
@@ -10,23 +10,37 @@ admin.initializeApp(functions.config().firebase);
 //  response.send("Hello from Firebase!");
 // });
 
-exports.sendWelcomeNotification = functions.auth.user().onCreate(async user => {
-  try {
-    const uid = user.uid;
-    const doc = await firebase
-      .firestore()
-      .collection("notificationTokens")
-      .doc(uid);
-    const payload = {
+
+exports.sendPushNotification = functions.firestore
+  .document("notificationTokens")
+  .onCreate(event => {
+    // gets standard JavaScript object from the new write
+    const writeData = event.data.data();
+    // access data necessary for push notification
+    const sender = writeData.uid;
+    const senderName = writeData.name;
+    const recipient = writeData.recipient;
+    // the payload is what will be delivered to the device(s)
+    let payload = {
       notification: {
-        title: "Hoşgeldiniz",
-        body: "Hadimaça uygulamasını kullandığınız için teşekkür ederiz."
+        title: "Başlık",
+        body: "Mesaj"
       }
     };
-
-    const { token } = doc;
-    return admin.messaging().sendToDevice(token, payload);
-  } catch (error) {
-    console.log("Error while sending welcome notification", error.message);
-  }
-});
+    // either store the recepient tokens in the document write
+    const tokens = writeData.tokens;
+    
+    // or collect them by accessing your database
+    var pushToken = "";
+    return functions.auth.user().onCreate(user => {
+      const uid = user.uid;
+      return functions.firestore
+        .collection("notificationTokens/" + uid)
+        .get()
+        .then(doc => {
+          pushToken = doc.data().token;
+          // sendToDevice can also accept an array of push tokens
+          return admin.messaging().sendToDevice(pushToken, payload);
+        });
+    });
+  });
