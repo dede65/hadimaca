e515d6564d3555a57caea589d86149764a2aaa5b
dede65 <dede.ugur65@gmail.com>
2019-07-01 00:48:26 +0300

    add player details

--- src/screens/PlayerDetails.js
+++ src/screens/PlayerDetails.js
@@ -1,11 +1,120 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
 
 class PlayerDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
+    const { navigation } = this.props;
+    const player = navigation.getParam("player");
+    console.log("Player details:", player);
+    const {
+      firstName,
+      lastName,
+      age,
+      foot,
+      height,
+      position,
+      weight,
+      birthday,
+      playerProfileImageURL
+    } = player;
     return (
       <View style={styles.container}>
-        <Text>Oyuncu Detayları</Text>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>{firstName + " " + lastName}</Text>
+        </View>
+        <ScrollView style={styles.scrollview}>
+          <View style={styles.playerInfoContainer}>
+            <View style={styles.imageContainer}>
+              <Image
+                style={styles.image}
+                source={{ uri: playerProfileImageURL }}
+              />
+            </View>
+            <View style={styles.details}>
+              <View style={styles.playerTeam}>
+                <Image style={styles.playerTeamLogo} />
+                <Text>Takımı</Text>
+              </View>
+              <View />
+              <View style={styles.info}>
+                <View style={{ flex: 1 }}>
+                  <Text>Ad</Text>
+                  <Text>Soyad</Text>
+                  <Text>Yaş</Text>
+                  <Text>Doğum tarihi</Text>
+                  <Text>Mevki</Text>
+                  <Text>Boy</Text>
+                  <Text>Kilo</Text>
+                  <Text>Ayak</Text>
+                </View>
+                <View style={{ flex: 1 }}>
+                  <Text>{firstName}</Text>
+                  <Text>{lastName}</Text>
+                  <Text>{age}</Text>
+                  <Text>{birthday}</Text>
+                  <Text>{position}</Text>
+                  <Text>{height}</Text>
+                  <Text>{weight}</Text>
+                  <Text>{foot}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View style={styles.previousTeams}>
+            <Text>Önceki oynadığı takımlar</Text>
+            <Text>---</Text>
+            <Text>---</Text>
+            <Text>---</Text>
+            <Text>---</Text>
+          </View>
+
+          <View style={styles.statistics}>
+            <Text>İstatistikler</Text>
+            <Text>Maç sayısı</Text>
+            <Text>Gol sayısı</Text>
+            <Text>Asist sayısı</Text>
+          </View>
+
+          <View style={styles.comments}>
+            <Text>Yorumlar</Text>
+            <View>
+              <Text>yorum 1</Text>
+            </View>
+            <View>
+              <Text>yorum 2</Text>
+            </View>
+            <View>
+              <Text>yorum 3</Text>
+            </View>
+            <View>
+              <Text>yorum 4</Text>
+            </View>
+            <View>
+              <Text>yorum 4</Text>
+            </View>
+          </View>
+        </ScrollView>
+        <View style={styles.comment}>
+          <TextInput placeholder="Yorum yap" style={styles.commentTextInput} />
+          <TouchableOpacity style={styles.sendButton}>
+            <Text>Gönder</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -14,8 +123,90 @@ export default PlayerDetails;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
+  },
+  playerInfoContainer: {
+    flexDirection: "row",
+    height: 200,
+    backgroundColor: "blue",
+    padding: 8,
+    margin: 8
+  },
+  header: {
+    backgroundColor: "green",
+    height: 64,
     alignItems: "center",
     justifyContent: "center"
+  },
+  headerTitle: { color: "#fff", fontSize: 18 },
+  scrollview: { marginBottom: 60 },
+  imageContainer: {
+    backgroundColor: "red",
+    padding: 2
+  },
+  image: {
+    flex: 1,
+    //height: 200,
+    width: 100,
+    backgroundColor: "yellow"
+  },
+  details: {
+    flex: 1,
+    backgroundColor: "yellow",
+    padding: 2
+  },
+  playerTeam: {
+    backgroundColor: "grey",
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "green"
+  },
+  playerTeamLogo: {
+    backgroundColor: "blue",
+    height: 30,
+    width: 30,
+    marginRight: 8
+  },
+  info: {
+    flexDirection: "row"
+  },
+  previousTeams: {
+    padding: 8,
+    margin: 8,
+    backgroundColor: "red"
+  },
+  statistics: {
+    backgroundColor: "grey",
+    padding: 4,
+    margin: 8
+  },
+  comments: {
+    backgroundColor: "yellow",
+    padding: 8,
+    margin: 8
+  },
+  comment: {
+    position: "absolute",
+    bottom: 0,
+    flexDirection: "row",
+    padding: 4,
+    margin: 8,
+    backgroundColor: "#f5f5f5"
+  },
+  commentTextInput: {
+    borderColor: "green",
+    borderWidth: 1,
+    width: "75%",
+    borderRadius: 24,
+    margin: 2
+  },
+  sendButton: {
+    flex: 1,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 24,
+    margin: 2
   }
 });
