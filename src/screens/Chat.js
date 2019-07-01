6783ac5e7da0bbc93491b25b8645f9d7d84b06cb
dede65 <dede.ugur65@gmail.com>
2019-07-01 23:27:52 +0300

    add chat screen

---
+++ src/screens/Chat.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+
+class Chat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: []
+    };
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any"
+          }
+        }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1
+          }}
+        />
+      </View>
+    );
+  }
+}
+export default Chat;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
