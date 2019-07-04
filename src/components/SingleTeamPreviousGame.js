33210e285967e32167beea3781a24a691b970b25
dede65 <dede.ugur65@gmail.com>
2019-07-05 00:34:46 +0300

    add fake data and change some style

--- src/components/SingleTeamPreviousGame.js
+++ src/components/SingleTeamPreviousGame.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 class SingleTeamPreviousGame extends Component {
   render() {
+    const { previousGame } = this.props;
     return (
       <View style={styles.container}>
         <TouchableOpacity style={styles.content}>
@@ -11,22 +12,24 @@ class SingleTeamPreviousGame extends Component {
               <Image style={styles.homeTeamLogo} />
             </View>
             <View style={styles.homeTeamTitle}>
-              <Text>Ev sahibi</Text>
+              <Text>{previousGame.homeTeam.name}</Text>
             </View>
-            <View style={styles.homeTeamScore}>2</View>
+            <View style={styles.homeTeamScore}>
+              <Text>{previousGame.homeTeam.score}</Text>
+            </View>
           </View>
           <View style={styles.seperator}>
             <Text>-</Text>
           </View>
           <View style={styles.awayTeam}>
-            <View style={styles.awayTeamLogoContainer}>
-              <Image style={styles.awayTeamLogo} />
+            <View style={styles.awayTeamScore}>
+              <Text>{previousGame.awayTeam.score}</Text>
             </View>
             <View style={styles.awayTeamTitle}>
-              <Text>Deplasman</Text>
+              <Text>{previousGame.awayTeam.name}</Text>
             </View>
-            <View style={styles.awayTeamScore}>
-              <Text>1</Text>
+            <View style={styles.awayTeamLogoContainer}>
+              <Image style={styles.awayTeamLogo} />
             </View>
           </View>
         </TouchableOpacity>
@@ -42,11 +45,15 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 1,
-    flexDirection: "row"
+    flexDirection: "row",
+    marginBottom: 4
   },
   homeTeam: {
-    flex: 3,
-    flexDirection: "row"
+    backgroundColor: "yellow",
+    flex: 4,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around"
   },
   homeTeamLogoContainer: {
     alignItems: "center",
@@ -57,17 +64,23 @@ const styles = StyleSheet.create({
     width: 30,
     borderRadius: 15,
     borderWidth: 2,
-    borderColor: "#fff"
+    borderColor: "#fff",
+    backgroundColor: "grey"
   },
   homeTeamTitle: {},
   homeTeamScore: {},
   seperator: {
-    flex: 1
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "grey"
   },
 
   awayTeam: {
-    flex: 3,
-    flexDirection: "row"
+    backgroundColor: "yellow",
+    flex: 4,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around"
   },
   awayTeamLogoContainer: {
     alignItems: "center",
@@ -78,7 +91,8 @@ const styles = StyleSheet.create({
     width: 30,
     borderRadius: 15,
     borderWidth: 2,
-    borderColor: "#fff"
+    borderColor: "#fff",
+    backgroundColor: "grey"
   },
   awayTeamTitle: {},
   awayTeamScore: {}
