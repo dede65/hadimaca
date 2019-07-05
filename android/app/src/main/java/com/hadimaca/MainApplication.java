a0a7ffd62e22c3d011ad0d652a0ca584035ae4f3
dede65 <dede.ugur65@gmail.com>
2019-07-05 20:45:34 +0300

    add NetInfoPackage()

--- android/app/src/main/java/com/hadimaca/MainApplication.java
+++ android/app/src/main/java/com/hadimaca/MainApplication.java
@@ -3,6 +3,7 @@ package com.hadimaca;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
@@ -45,6 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new NetInfoPackage(),
             new ImagePickerPackage(),
             new FBSDKPackage(mCallbackManager),
             new RNFirebasePackage(),
