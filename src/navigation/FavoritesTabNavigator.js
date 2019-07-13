a38f0e09cdbd213dcc8132d1365aaa46720ffdee
dede65 <dede.ugur65@gmail.com>
2019-07-13 17:26:28 +0300

    change tabbar style

--- src/navigation/FavoritesTabNavigator.js
+++ src/navigation/FavoritesTabNavigator.js
@@ -1,36 +1,41 @@
 import React, { Component } from "react";
-import { createMaterialTopTabNavigator, createStackNavigator } from "react-navigation";
+import {
+  createMaterialTopTabNavigator,
+  createStackNavigator
+} from "react-navigation";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import FavoriteTeams from "../screens/tabs/maintabs/FavoriteTeams";
 import FavoritePlayers from "../screens/FavoritePlayers";
 
-const FavoritesTabNavigator = createMaterialTopTabNavigator({
-  FavoriteTeamsScreen: {
-    screen: FavoriteTeams,
-    navigationOptions: {
-      tabBarLabel: "Takımlar",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="view-list" color={tintColor} size={24} />
-      )
+const FavoritesTabNavigator = createMaterialTopTabNavigator(
+  {
+    FavoriteTeamsScreen: {
+      screen: FavoriteTeams,
+      navigationOptions: {
+        tabBarLabel: "Takımlar",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="view-list" color={tintColor} size={24} />
+        )
+      }
+    },
+    FavoritePlayersScreen: {
+      screen: FavoritePlayers,
+      navigationOptions: {
+        tabBarLabel: "Oyuncular",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="people" color={tintColor} size={24} />
+        )
+      }
     }
   },
-  FavoritePlayersScreen: {
-    screen: FavoritePlayers,
-    navigationOptions: {
-      tabBarLabel: "Oyuncular",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="people" color={tintColor} size={24} />
-      )
-    }
-  }
-},{
+  {
     lazy: false,
     backBehavior: "none",
     tabBarPosition: "top",
     tabBarOptions: {
+      upperCaseLabel:false,
       tabStyle: {
         height: 64,
-        backgroundColor: "green"
       },
       labelStyle: {
         fontSize: 10
@@ -44,8 +49,9 @@ const FavoritesTabNavigator = createMaterialTopTabNavigator({
         height: 0
       },
       showIcon: true,
-      showLabel: false
+      showLabel: true
     }
-  });
+  }
+);
 
 export default FavoritesTabNavigator;
