eb893ce83453906d7a841bcb475c10a953e26aa3
dede65 <dede.ugur65@gmail.com>
2019-07-14 12:19:12 +0300

    add back button to header

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet,TouchableOpacity } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 class Chat extends Component {
   constructor(props) {
@@ -37,8 +38,22 @@ class Chat extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={styles.headerTitle}>Mesajlar</Text>
+      <View style={styles.header}>
+          <View style={styles.headerLeftButton}>
+            <TouchableOpacity
+              onPress={() => {
+                this.props.navigation.goBack();
+              }}
+            >
+              <Icon name="chevron-left" color="white" size={48} />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{ flex: 3, alignItems: "center", justifyContent: "center" }}
+          >
+            <Text style={styles.headerTitle}>Mesajlar</Text>
+          </View>
+          <View style={styles.headerRightButton} />
         </View>
         <View style={{ flex: 1 }}>
           <GiftedChat
@@ -60,10 +75,16 @@ const styles = StyleSheet.create({
     flex: 1
   },
   header: {
+    flexDirection: "row",
     backgroundColor: "green",
     height: 64,
     alignItems: "center",
     justifyContent: "center"
   },
-  headerTitle: { color: "#fff", fontSize: 18 }
+  headerLeftButton: {
+    //backgroundColor: "yellow",
+    flex: 1
+  },
+  headerTitle: { color: "#fff", fontSize: 18 },
+  headerRightButton: { backgroundColor: "yellow", flex: 1 },
 });
